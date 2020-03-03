2a5961db07806a605ea523add0dde24237a3294d
ajay-k <ajay_k@sbcglobal.net>
2020-03-03 10:56:45 -0800

    Event listener for all buttons working

--- js/app.js
+++ js/app.js
@@ -1,8 +1,33 @@
 console.log("Linked and working!")
 
-const btnA = document.querySelector("button");
 
-console.log(btnA);
+//document.querySelectorAll("button")[0]
+
+const word = ["C", "A", "B"];
+
+//Gets all the divs
+let wordBankDivs = document.getElementsByClassName("wordBank")[0];
+
+let hangmanImageSources = ["images/drawHead.png", "images/drawBody.png",
+ "images/drawLeftLeg.png", "images/drawRightLeg.png", "images/drawLeftArm.png","images/drawRightArm.png"];
+
+ let hanganImageObj = document.getElementsByTagName("img")[0];
+
+console.log(hanganImageObj);
+ 
+
+
+//wordBankDivs.children[2].textContent = "T";
+
+//grab input from user 
+//Let's say it's A
+
+//check if it's part of the word array
+// console.log(word.indexOf("A"));
+// if(word.indexOf("A") !== -1){
+// 	let myIndex = word.indexOf("A");
+// 	wordBankDivs.children[myIndex].textContent = word[myIndex];
+//}
 
 function popOut(evt){
 	alert("HELLO");
@@ -16,6 +41,107 @@ function popOut(evt){
 
 // })
 
-btnA.addEventListener('click', function() {
-	alert('title click!');
-});
+// const btnA = document.querySelector("button");
+
+// btnA.addEventListener('click', function() {
+// 	//alert('Clicked A!');
+// 	//wordBankDivs.children[2].textContent = "M";
+// 	if(word.indexOf(this.value) !== -1){
+// 	let myIndex = word.indexOf(this.value);
+// 	wordBankDivs.children[myIndex].textContent = word[myIndex];
+// }
+
+// });
+
+// //document.querySelectorAll("button")[0]
+// const btnB = document.querySelectorAll("button")[1];
+
+// btnB.addEventListener('click', function() {
+// 	//alert('Clicked B!');
+// 	//wordBankDivs.children[2].textContent = "M";
+// 	if(word.indexOf(this.value) !== -1){
+// 	let myIndex = word.indexOf(this.value);
+// 	wordBankDivs.children[myIndex].textContent = word[myIndex];
+// 	}
+
+// });
+
+// const btnC = document.querySelectorAll("button")[2];
+
+
+// btnC.addEventListener('click', function() {
+// 	//alert('Clicked C!');
+// 	//wordBankDivs.children[2].textContent = "M";
+// 	if(word.indexOf(this.value) !== -1){
+// 	let myIndex = word.indexOf(this.value);
+// 	wordBankDivs.children[myIndex].textContent = word[myIndex];
+// 	}
+
+// });
+
+// const btnT = document.querySelectorAll("button")[3];
+
+
+// btnT.addEventListener('click', function() {
+// 	//alert('Clicked C!');
+// 	//wordBankDivs.children[2].textContent = "M";
+// 	if(word.indexOf(this.value) !== -1){
+// 	let myIndex = word.indexOf(this.value);
+// 	wordBankDivs.children[myIndex].textContent = word[myIndex];
+// 	}else{
+		// hanganImageObj.src = hangmanImageSources[0];
+		// hangmanImageSources.shift();
+// 	}
+
+// });
+
+var letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+for(i = 0; i < letters.length; i++) {
+    var button = document.createElement("button");
+    button.textContent = letters[i];
+    button.value = letters[i];
+    //button.innerHTML = Chapter[i];
+    //button.className = "btn btn-outline-success";
+    //let buttonDiv = document.getElementsByClassName("keyboard");
+    //document.getElementById("id").appendChild(element);
+    //document.getElementsByClassName("wordBank")[0].appendChild("ABC");
+    //document.getElementsByClassName("keyboard").appendChild("ABC");
+    //$(".keyboard").append(button);
+    document.getElementsByClassName("keyboard")[0].append(button);
+  }
+
+
+
+const btnA = document.querySelector("button");
+
+// btnA.addEventListener('click', function() {
+// 	//alert('Clicked A!');
+// 	//wordBankDivs.children[2].textContent = "M";
+	// if(word.indexOf(this.value) !== -1){
+	// let myIndex = word.indexOf(this.value);
+	// wordBankDivs.children[myIndex].textContent = word[myIndex];
+// }
+
+// });
+
+let buttons = document.querySelectorAll(".keyboard button");
+console.log("Buttons: " ,buttons);
+//var i = 0, length = buttons.length;
+for (let i=0; i < buttons.length; i++) {
+
+   buttons[i].addEventListener("click", function() {
+        // use keyword this to target clicked button
+        //alert(buttons[i].value);
+        if(word.indexOf(buttons[i].value) !== -1){
+			let myIndex = word.indexOf(buttons[i].value);
+			wordBankDivs.children[myIndex].textContent = word[myIndex];
+			buttons[i].style.color = "green";
+		}else{
+			hanganImageObj.src = hangmanImageSources[0];
+			hangmanImageSources.shift();
+		}
+		buttons[i].disabled = true
+
+    });
+
+};
